87fac301c7c20b98865e36b4b45ad695beba2e67
Sergei Kovalenko <sergei.kovalenko@aeriagames.com>
2019-09-20 16:25:17 +0200

    additional load in whatever controller

--- apidemo/Controllers/WhateverController.cs
+++ apidemo/Controllers/WhateverController.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,14 +8,20 @@ namespace ApiDemo.Controllers
     [ApiController]
     public class WhateverController : ControllerBase
     {
-        [HttpGet]
+        [HttpGet("{param}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public string Process(string param)
         {
             string result = "";
+            Random random = new Random();
             foreach (char c in param)
             {
-                int x = c;
+                int x = (int) c;
+                x *= random.Next(0, 1000);
+                while (x > 127)
+                {
+                    x /= 2;
+                }
                 result += x.ToString();
             }
             return result;
